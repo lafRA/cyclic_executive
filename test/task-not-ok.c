58ad9b3862b4c9c1a21f69fa9153775ab4894f57
Daniele Molinari <daniele.molinari6@gmail.com>
2012-05-24 13:31:36 +0200

    task set completi

--- test/task-not-ok.c
+++ test/task-not-ok.c
@@ -7,13 +7,16 @@
 #define H_PERIOD_ 20
 
 /* Numero di frame */
-#define NUM_FRAMES_ 5
+#define NUM_FRAMES_ 4
 
 /* Numero di task */
-#define NUM_P_TASKS_ 2
+#define NUM_P_TASKS_ 4
 
-void task0_code();
 void task1_code();
+void task2_code();
+void task3_code();
+void task4_code();
+
 
 void ap_task_code();
 
@@ -29,101 +32,113 @@ task_routine AP_TASK;
 int * SCHEDULE[NUM_FRAMES_];
 int SLACK[NUM_FRAMES_];
 
-void task_init()
-  {
-  /* Inizializzazione di P_TASKS[] */
-  P_TASKS[0] = task0_code;
-  P_TASKS[1] = task1_code;
-  /* ... */
-  
-  /* Inizializzazione di AP_TASK */
-  AP_TASK = ap_task_code;
+void task_init() {
+	/* Inizializzazione di P_TASKS[] */
+	P_TASKS[1] = task1_code;
+	P_TASKS[2] = task2_code;
+	P_TASKS[3] = task3_code;
+	P_TASKS[4] = task4_code;
+	
+	/* ... */
+
+	/* Inizializzazione di AP_TASK */
+	AP_TASK = ap_task_code;
 
 
-  /* Inizializzazione di SCHEDULE e SLACK (se necessario) */
+	/* Inizializzazione di SCHEDULE e SLACK (se necessario) */
 
-  /* frame 0 */
-  SCHEDULE[0] = (int *) malloc( sizeof( int ) * 1 );
-  SCHEDULE[0][0] = -1;
+	/* frame 0 */
+	SCHEDULE[0] = (int *) malloc( sizeof( int ) * 4 );
+	SCHEDULE[0][0] = 1;
+	SCHEDULE[0][1] = 2;
+	SCHEDULE[0][2] = 4;
+	SCHEDULE[0][3] = -1;
 
-  SLACK[0] = 4; /* tutto il frame */
-
-
-  /* frame 1 */
-  SCHEDULE[1] = (int *) malloc( sizeof( int ) * 1 );
-  SCHEDULE[1][0] = -1;
-
-  SLACK[1] = 4; /* tutto il frame */
+	SLACK[0] = 1; /* tutto il frame */
 
 
-  /* frame 2 */
-  SCHEDULE[2] = (int *) malloc( sizeof( int ) * 1 );
-  SCHEDULE[2][0] = -1;
+	/* frame 1 */
+	SCHEDULE[1] = (int *) malloc( sizeof( int ) * 3 );
+	SCHEDULE[1][0] = 3;
+	SCHEDULE[1][1] = 2;
+	SCHEDULE[1][2] = -1;
 
-  SLACK[2] = 4; /* tutto il frame */
-
-
-  /* frame 3 */
-  SCHEDULE[3] = (int *) malloc( sizeof( int ) * 1 );
-  SCHEDULE[3][0] = -1;
-
-  SLACK[3] = 4; /* tutto il frame */
+	SLACK[1] = 0; /* tutto il frame */
 
 
-  /* frame 4 */
-  SCHEDULE[4] = (int *) malloc( sizeof( int ) * 1 );
-  SCHEDULE[4][0] = -1;
-  
-  SLACK[4] = 4; /* tutto il frame */
+	/* frame 2 */
+	SCHEDULE[2] = (int *) malloc( sizeof( int ) * 2 );
+	SCHEDULE[2][0] = 1;
+	SCHEDULE[2][1] = -1;
 
-  /* Custom Code */
-  }
+	SLACK[2] = 2; /* tutto il frame */
 
-void task_destroy()
-  {
-  unsigned int i;
 
-  /* Custom Code */
+	/* frame 3 */
+	SCHEDULE[3] = (int *) malloc( sizeof( int ) * 2 );
+	SCHEDULE[3][0] = 2;
+	SCHEDULE[3][1] = -1;
 
-  for ( i = 0; i < NUM_FRAMES; ++i )
-    free( SCHEDULE[i] );
-  }
+	SLACK[3] = 3; /* tutto il frame */
+}
+
+void task_destroy() {
+	unsigned int i;
+
+	/* Custom Code */
+	for ( i = 0; i < NUM_FRAMES; ++i )
+		free( SCHEDULE[i] );
+}
 
 /**********************************************************/
 
-void busy_wait(unsigned int millisec)
-  {
-  struct timeval actual;
-  struct timeval final;
+void busy_wait(unsigned int best, unsigned int worst) {
+	struct timeval actual;
+	struct timeval final;
+	
+	gettimeofday(&actual, NULL);
+	
+	unsigned int millisec = best + rand() % (worst - best + 1);
+	
+	final.tv_sec = actual.tv_sec + (actual.tv_usec + millisec * 1000) / 1000000;
+	final.tv_usec = (actual.tv_usec + millisec * 1000) % 1000000;
+	
+	do {
+		gettimeofday(&actual, NULL);
+	} while (actual.tv_sec < final.tv_sec || (actual.tv_sec == final.tv_sec && actual.tv_usec < final.tv_usec));
+}
 
-  gettimeofday(&actual, NULL);
-
-  final.tv_sec = actual.tv_sec + (actual.tv_usec + millisec * 1000) / 1000000;
-  final.tv_usec = (actual.tv_usec + millisec * 1000) % 1000000;
-
-  do
-    {
-    gettimeofday(&actual, NULL);
-    }
-  while (actual.tv_sec < final.tv_sec || (actual.tv_sec == final.tv_sec && actual.tv_usec < final.tv_usec));
-  }
-  
 /**********************************************************/
 
 /* Nota: nel codice dei task e' lecito chiamare ap_task_request() */
 
- void task0_code()
-   {
-   /* Custom Code */
-   }
+void task1_code() {
+	/* Custom Code */
+	busy_wait(15, 20);
+}
 
-  void task1_code()
-   {
-   /* Custom Code */
-   }
+void task2_code() {
+	/* Custom Code */
+	static unsigned int count = 0;
+	busy_wait(5, 10);
+	
+	++count;
+	if(count % 3 > 0) {
+		ap_task_request();
+	}
+}
 
+void task3_code() {
+	/* Custom Code */
+	busy_wait(40, 50);
+}
 
-  void ap_task_code()
-   {
-   /* Custom Code */
-   }
+void task4_code() {
+	/* Custom Code */
+	busy_wait(5, 10);
+}
+
+void ap_task_code() {
+	/* Custom Code */
+	busy_wait(10, 10);
+}
